f5c9b3815b85a316249659e162af543d5edade18
Kim Kern <github@kimkern.de>
2018-05-19 19:47:31 +0200

    remove login method from UserService

--- src/users/dto/user-login-request.dto.ts
+++
@@ -1,9 +0,0 @@
-import { IsString } from 'class-validator';
-
-export class UserLoginRequestDto {
-  @IsString()
-  readonly username: string;
-
-  @IsString()
-  readonly password: string;
-}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,4 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
-import { UserLoginRequestDto } from './dto/user-login-request.dto';
-import { UserCreateResponseDto } from './dto/user-create-response.dto';
 import { UserCreateRequestDto } from './dto/user-create-request.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model, Types } from 'mongoose';
@@ -10,14 +8,6 @@ import { IUser } from './interfaces/user.interface';
 export class UsersService {
 
   constructor(@InjectModel('User') private readonly userModel: Model<IUser>) {
-  }
-
-  async login(loginUser: UserLoginRequestDto): Promise<UserCreateResponseDto> {
-    const user: IUser = await this.userModel.findOne({username: loginUser.username});
-    if (!user) {
-      throw new Error('User not found.');
-    }
-    return { username: 'user', jwt: 'jwt' };
   }
 
   async createUser(user: UserCreateRequestDto): Promise<IUser> {
