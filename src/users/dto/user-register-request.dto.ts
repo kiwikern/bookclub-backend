6888dc20462c649079eed012fa99c71df6691106
Kim Kern <github@kimkern.de>
2018-05-19 18:08:17 +0200

    add additional user register validation

--- src/users/dto/user-register-request.dto.ts
+++ src/users/dto/user-register-request.dto.ts
@@ -1,13 +1,16 @@
-import { IsOptional, IsString } from 'class-validator';
+import { IsEmail, IsOptional, IsString, Length } from 'class-validator';
 
 export class UserRegisterRequestDto {
   @IsString()
+  @Length(3, 30)
   readonly username: string;
 
   @IsString()
+  @Length(6, 30)
   readonly password: string;
 
   @IsString()
+  @IsEmail()
   @IsOptional()
   readonly email: string;
 }
