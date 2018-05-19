75797157fa759557dff778ab1e41c5b7d35cdaa7
Kim Kern <github@kimkern.de>
2018-05-19 19:44:38 +0200

    make index for user email sparse
    mail addresses should be unique but null is allowed as duplicate

--- src/users/schemas/user.schema.ts
+++ src/users/schemas/user.schema.ts
@@ -3,6 +3,6 @@ import { Schema } from 'mongoose';
 export const UserSchema = new Schema({
   username: { type: String, unique: true },
   password: String,
-  email: { type: String, unique: true },
+  email: { type: String, unique: true, sparse: true },
   name: String,
 });
